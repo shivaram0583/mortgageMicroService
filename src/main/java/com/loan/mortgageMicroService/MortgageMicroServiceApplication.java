400363a6298bce1c6b94089a1e3594523285e56a
shivaram0583 <dvvshivaram@gmail.com>
2022-12-15 12:26:27 +0530

    added logs

--- src/main/java/com/loan/mortgageMicroService/MortgageMicroServiceApplication.java
+++ src/main/java/com/loan/mortgageMicroService/MortgageMicroServiceApplication.java
@@ -2,6 +2,7 @@ package com.loan.mortgageMicroService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class MortgageMicroServiceApplication {
